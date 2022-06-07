d0d8f559d3ebbf14debd1ec512b393b81ca1f4b7
subhanrizwan0 <subhanahmed444sherazi@gmail.com>
2022-06-08 01:52:45 +0500

    jsassignment5

---
+++ index.js
@@ -0,0 +1,146 @@
+// question# 01
+// var arr=[[4,5],[20,21],[40,1],[52,3]]
+// console.log(arr[3][0]);
+
+// // question#2
+// // var arr=[[0,1,2],[1,0,1,2],[2,1,0,1]]
+// // for(var i=0; i<arr.length; i++){
+// //     for(var j=0 ; arr[i].length;j++){
+// //         document.write((arr[i][j]))
+
+//                         // 0 1  C:\Users\user 2\Desktop\assignment-5\chap17to20\chap17to20.html
+//                         // 
+//                         // 0 2
+//                         // 0 3
+//                         // 0 4
+//     }
+ 
+
+// }
+// 
+// question# 3
+// var arr=[]
+// for(i=10;i>=0;i--){
+//     console.log(i);
+// }
+
+// var arr=[]
+// for(i=0;i<=10;i++){
+//     console.log(i);
+//     if(i===5)
+//     break
+// }
+
+// question# 4
+// var table=prompt("enter multiplication table")
+// for(i=1; i <= 15; i++ )
+// console.log(table + "x" , i + "=" + table*i  );
+
+// question#5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+// for (f = 0; f < fruits.length; f++) {
+//     document.write(fruits[f] +"<br>")
+//     document.write("element at index " + f + " is " + fruits[f] + "<br>");
+    
+//     console.log(fruits);
+    
+// } 
+
+// question# 06
+// var arr=[]
+// for(n=0; n<=10; n++){
+//     console.log(n);
+// }
+
+// var arr=[]
+// for(n=10; n>=0; n--){
+//     console.log(n);
+// }
+
+
+// for(n=0; n<=20; n++){
+// if(n%2==0)
+//     console.log(n);
+// }
+
+// for(n=2; n<=21; n++){
+//     if(n%2 ==0)
+//         console.log(n);
+//     }
+
+// for(n=2; n<=21; n++){
+//   if(n%2 ==0)
+//     console.log(n+"k");
+
+//}question#7
+// var cities = ["cake" , "apple pie" , "cookie" ,"chips","patties"]
+// var cityName = prompt("Enter Name")
+// var flag = false
+
+// for(var i = 0; i < cities.length; i++){
+// if (cityName === cities[i]){
+// flag = true
+
+
+
+// }
+
+// } if(flag==="yes"){
+//     alert("yes")
+// }else(alert("no"))
+
+
+// question # 08
+// var arr=[25,50,75,100]
+// var max=0
+// for(var i=0; i<=arr.length; i++){
+//     if(max<arr[i]){
+//         max=arr[i];
+        
+        
+//     }
+// }
+// console.log(max)
+
+// question#09
+ var arr=[25,50,75,10]
+var min=0
+for(var i=0; i+1 > arr.length; i++){
+    if(min > arr[i]){
+        min=arr[i];
+        
+        
+    }
+    console.log(min);
+}
+
+// or
+// var arr=[23,33,44,56]
+// var min=arr[0]
+// for(var i=0; i<arr.length; i++){
+// if(min>arr[i]){
+//     min=arr[i]
+// }console.log(min);
+// }
+
+
+
+
+
+// question#10
+
+// var table=prompt("enter num");
+// for(var i=1; i<=20; i++){
+//     console.log(table*i );
+    
+// }
+
+
+
+
+
+
+
+
+
+
